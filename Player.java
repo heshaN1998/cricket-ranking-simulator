976d2f7d00c35bbd6fffe64cee48bf267cbb013e
heshan <heshansandakelum2018@gmail.com>
2026-05-07 23:24:52 +0530

    adding strike rate and economy data to player

--- Player.java
+++ Player.java
@@ -4,6 +4,12 @@ public class Player {
     private String team;
     private String type;
     private int points;
+
+    private int runs;
+private int ballsFaced;
+
+private int runsGiven;
+private int ballsBowled;
     
     public Player(String name,String team,String type,int points){
         this.name=name;
@@ -31,6 +37,32 @@ public class Player {
     public String toString(){
         return name + " | " + team +" | " + type +" | ponits: "+points;
     }
-   
+   public void setBattingStats(int runs, int ballsFaced) {
+    this.runs = runs;
+    this.ballsFaced = ballsFaced;
+}
+
+public void setBowlingStats(int runsGiven, int ballsBowled) {
+    this.runsGiven = runsGiven;
+    this.ballsBowled = ballsBowled;
+}
+public double getStrikeRate() {
+
+    if (ballsFaced == 0) {
+        return 0;
+    }
+
+    return (runs * 100.0) / ballsFaced;
+}
+public double getEconomy() {
+
+    if (ballsBowled == 0) {
+        return 0;
+    }
+
+    double overs = ballsBowled / 6.0;
+
+    return runsGiven / overs;
+}
 
 }
